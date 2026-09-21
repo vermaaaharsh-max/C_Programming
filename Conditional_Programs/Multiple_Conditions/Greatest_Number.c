af6249bdf587dd2d50165a50813a49959117ce4f
Harsh Verma <vermaaaharsh@gmail.com>
2026-09-22 01:54:28 +0530

    Updated Program to Find Greatest Number among three Numbers

--- Conditional_Programs/Multiple_Conditions/Greatest_Number.c
+++ Conditional_Programs/Multiple_Conditions/Greatest_Number.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main() {
     int num1, num2, num3;
-    printf("Enter three integers: ");
-    scanf("%d %d %d", &num1, &num2, &num3);
+    printf("Enter first Number: ");
+    scanf("%d", &num1);
+    printf("Enter second Number: ");
+    scanf("%d", &num2);
+    printf("Enter third Number: ");
+    scanf("%d", &num3);
 
     if (num1 > num2 && num1 > num3) {
         printf("num1 = %d is the greatest number.\n", num1);
