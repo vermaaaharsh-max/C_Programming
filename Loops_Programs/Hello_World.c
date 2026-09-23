ecd2dc49c7d5a740fab3aa05d9c87d79b51cc8a3
Harsh Verma <vermaaaharsh@gmail.com>
2026-09-24 00:34:36 +0530

    Add Program to print Hello World as much user want to by using Loop concept.

---
+++ Loops_Programs/Hello_World.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter how many time you want to print Hello World: ");
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++) 
+    {printf("Hello WORLD\n");}
+}
