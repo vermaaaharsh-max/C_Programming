7ae65894e17cf6959dfa0661c8bffdb9a44700f6
Harsh Verma <vermaaaharsh@gmail.com>
2026-09-24 15:25:42 +0530

    Add program to do Back Counting from 100 till positive numbers by following the series.

---
+++ Loops_Programs/Back_Counting.c
@@ -0,0 +1,10 @@
+//Back counting till positive number from 100 
+// 100,97,94,91
+#include <stdio.h>
+int main() {
+   int a = 100;
+for (int i =1; a> 0; i++)
+{printf("%d\n",a);
+    a = a-3;
+} return 0;
+}
